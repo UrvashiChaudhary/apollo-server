1bc1f285036a9c29312226691f91bf964f7620e2
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2021-01-27 21:54:16 +0530

    updated

--- src/modules/user/mutation.js
+++ src/modules/user/mutation.js
@@ -3,6 +3,8 @@ export default {
     const { payLoad: { email, password } } = args;
     const { dataSources: { userAPI } } = context;
     const response = await userAPI.loginUser({ email, password });
+    console.log('responseeeee', response);
+    // const res = JSON.stringify(response);
     return response.data;
   }
 };
