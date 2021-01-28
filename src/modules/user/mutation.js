8d659e0970d124c1edb93f5d824e6dd27ad0c0e2
Urvashi Chaudhary <urvashi.chaudhary@urvashic.successivetech.com>
2021-01-28 15:29:08 +0530

    Integration of apollo-client

--- src/modules/user/mutation.js
+++ src/modules/user/mutation.js
@@ -3,8 +3,6 @@ export default {
     const { payLoad: { email, password } } = args;
     const { dataSources: { userAPI } } = context;
     const response = await userAPI.loginUser({ email, password });
-    console.log('responseeeee', response);
-    // const res = JSON.stringify(response);
-    return response.data;
+    return response;
   }
 };
